7b8a8f30c43f4811352c30460a4aeb8831926b05
Chinh Do <cdo@vienxu.com>
2020-06-21 01:24:16 -0400

    Loop

--- sh/bandwidth.sh
+++ sh/bandwidth.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# Bandwidth Check: Check Real-Time WAN Bandwidth Usage
-# by WaLLy3K 11APR15 (Updated 25NOV15) for Tomato Firmware
-# Usage: "checkWAN <seconds>" (To check WAN over a period of X seconds and average the result)
+# Calculate and output real-time bandwidth usage
+# Based on script by WaLLy3K (see https://www.linksysinfo.org/index.php?threads/how-to-monitor-the-ip-traffic-and-bandwidth-with-cli.71998/)
+# For running on Tomato USB or Advanced Tomato routers
 
 wan_iface=`nvram get wan_iface`
-calc(){ awk "BEGIN { print $*}"; }    # Calculate floating point arithmetic using AWK instead of BC
+calc(){ awk "BEGIN { print $*}"; }    # Calculate floating point arithmetic using AWK instead o
 
 checkWAN () {
     [ -z $1 ] && sec="1" || sec="$1"
@@ -17,13 +17,13 @@ checkWAN () {
     cRX=$(echo $netdev | cut -d' ' -f2)
     cTX=$(echo $netdev | cut -d' ' -f10)
 
-    [ $cRX \< $pRX ] && getRX=`calc "$cRX + (0xFFFFFFFF - $pRX)"` || getRX=`calc "($cRX - $pRX)"`
-    [ $cTX \< $pTX ] && getTX=`calc "$cTX + (0xFFFFFFFF - $pTX)"` || getTX=`calc "($cTX - $pTX)"`
+    [ $cRX \< $pRX ] && getRX=`calc "$cRX + (0xFFFFFFFF - $pRX)"` || getRX=`calc "($cRX - $pRX)
+    [ $cTX \< $pTX ] && getTX=`calc "$cTX + (0xFFFFFFFF - $pTX)"` || getTX=`calc "($cTX - $pTX)
     dlBytes=$(($getRX/$sec)); ulBytes=$(($getTX/$sec))
     [ $dlBytes -le "12000" -a $ulBytes -le "4000" ] && wanStatus="idle" || wanStatus="busy"
 
-    getDLKbit=$(printf "%.0f\n" `calc $dlBytes*0.008`);        getULKbit=$(printf "%.0f\n" `calc $ulBytes*0.008`)
-    getDLMbit=$(printf "%.2f\n" `calc $dlBytes*0.000008`);    getULMbit=$(printf "%.2f\n" `calc $ulBytes*0.000008`)
+    getDLKbit=$(printf "%.0f\n" `calc $dlBytes*0.008`);        getULKbit=$(printf "%.0f\n" `cal
+    getDLMbit=$(printf "%.2f\n" `calc $dlBytes*0.000008`);    getULMbit=$(printf "%.2f\n" `calc
 }
 
 seconds=$1
@@ -32,8 +32,15 @@ then
   seconds=5
 fi
 
-checkWAN $seconds # Check WAN port for x seconds
-# [ $wanStatus = "idle" ] && echo "WAN: Idle" || echo "WAN: Busy (DL $getDLMbit Mbps / UL $getULMbit Mbps)"
+while :
+do
+  checkWAN 5
+  ts=`date +'%Y-%m-%dT%T'`
+  echo "$ts dl=$getDLMbit up=$getULMbit"
+done
 
-ts=`date +'%Y-%m-%dT%T'`
-echo "$ts dl=$getDLMbit up=$getULMbit"
+# checkWAN $seconds # Check WAN port for x seconds
+# [ $wanStatus = "idle" ] && echo "WAN: Idle" || echo "WAN: Busy (DL $getDLMbit Mbps / UL $getU
+
+# ts=`date +'%Y-%m-%dT%T'`
+# echo "$ts dl=$getDLMbit up=$getULMbit"
